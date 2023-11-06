922f88f628adc56bdb4fbea714b524379b2ffa16
sukeypark <hspark@data-alliance.com>
2023-11-06 16:05:43 +0900

    add application code

--- src/com/example/starbuzz/App.java
+++ src/com/example/starbuzz/App.java
@@ -1,7 +1,20 @@
 package com.example.starbuzz;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try (InputStream is = new LowerCaseInputStream(
+                new BufferedInputStream(new FileInputStream("resource/test.txt")))) {
+            int c;
+            while ((c = is.read()) >= 0) {
+                System.out.println((char) c);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
