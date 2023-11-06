10be7a8d126ed7224deb8cd145c34f5a9f3ab05b
sukeypark <hspark@data-alliance.com>
2023-11-06 15:15:40 +0900

    change return type to string

--- src/com/example/starbuzz/Beverage.java
+++ src/com/example/starbuzz/Beverage.java
@@ -4,8 +4,8 @@ public abstract class Beverage {
 
     protected String description = "Unknown Beverage";
 
-    public void getDescription() {
-        System.out.println(description);
+    public String getDescription() {
+        return description;
     }
 
     public abstract double cost();
